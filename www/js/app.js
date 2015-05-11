d2e617019020153d4b4ae43d4315132fa2c0d9f5
sakmor <koushouou@guangshangdePro.Home>
2015-05-12 01:43:01 +0800

    選單功能增加

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,5 @@
+var listData = [];
+
 function addTodoPicture() {
     navigator.camera.getPicture(addTodo, function () {
         alert("Failed to get camera.");
@@ -9,20 +11,39 @@ function addTodoPicture() {
     });
 }
 
+function selectList(title) {
+    this.url = title;
+    $("#qrcode").attr("src", "https://chart.googleapis.com/chart?chs=240x240&chld=H|1&cht=qr&chl=https://www.facebook.com/" + this.url);
+    $("#qrLogo").attr("src", "http://graph.facebook.com/" + this.url + "/picture?type=large");
+    document.getElementById("todo-title").value = this.url;
+    document.getElementById("todo-handel").innerText = "Edit Fan Page";
+    var a = document.getElementById("add-button");
+    a.defaultValue = "apple";
+    //    = this.url;
+
+    $("#add-button").attr("value", "Save Chagne");
+    logo = "http://graph.facebook.com/" + this.url + "/picture?type=large";
+    jQuery.getJSON('http://graph.facebook.com/' + this.url, 'Likes', function (result) {
+        document.getElementById("todo-body").value = result.likes + " 個人說讚";
+        //            myCounter.setValue(result.likes);
+        //        refreshIntervalId = setInterval(autoUpdata, 3000);
+    });
+
+};
+
 function addTodo(camera_url) {
     clearInterval(refreshIntervalId);
     var title = $("#todo-title").val();
     var body = $("#todo-body").val();
     var img_tag = "";
+
     img_tag = "<img src='" + logo + "'>";
 
-    var edit_button = "";
-    edit_button = '<a href="#add-page" data-icon="add" class="ui-btn-right">Add</a>';
-
+    document.getElementById("todo-handel").innerText = "Add New Fan Page";
     $.mobile.changePage($("#list-page"));
-    $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p>" +
-        edit_button + "</li>")
+    $("#todo-list").append("<li>" + ' <a href="#add-page" data-transition="slide" onclick="selectList(listData[' + listData.length + '])">   <img src="' + logo + '"><h1>' + title + '</h1></p>' + body + '</a>' + "</p></li>")
     $("#todo-list").listview('refresh');
+    listData[listData.length] = title;
 };
 
 i = 0;
@@ -49,7 +70,7 @@ $(document).ready(function () {
     $("#todo-title").keyup(function () {
 
         this.url = $("#todo-title").val();
-        $("#qrcode").attr("src", "https://chart.googleapis.com/chart?cht=qr&chs=240x240&chld=h&chl=https://www.facebook.com/" + this.url);
+        $("#qrcode").attr("src", "https://chart.googleapis.com/chart?chs=240x240&chld=H|1&cht=qr&chl=https://www.facebook.com/" + this.url);
         $("#qrLogo").attr("src", "http://graph.facebook.com/" + this.url + "/picture?type=large");
         logo = "http://graph.facebook.com/" + this.url + "/picture?type=large";
         jQuery.getJSON('http://graph.facebook.com/' + this.url, 'Likes', function (result) {
